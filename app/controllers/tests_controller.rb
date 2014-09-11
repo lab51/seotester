ff26ff0d1bb1ec4633d9aad3437094be344eb33f
lab51 <szymon.spychala@gmail.com>
2014-09-11 13:32:16 +0200

    Update tests_controller.rb

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,159 +3,150 @@ class TestsController < ApplicationController
   require 'open-uri'
   require 'json'
   require 'page_rankr'
-	require 'openssl'
-	require 'base64'
-	require 'cgi'
+  require 'openssl'
+  require 'base64'
+  require 'cgi'
   
-#city in title
-#te subdomeny
-
-	def index
-
-	end
+  def index
+  end
 
   def raport
-  	address_from_form = params[:address]	
-  	address_to_check = prepare_address(address_from_form)
-  	@show_address = address_to_check
-  	robots_address = "http://#{address_to_check}/robots.txt"
+    address_from_form = params[:address]	
+    address_to_check = prepare_address(address_from_form)
+    @show_address = address_to_check
+    robots_address = "http://#{address_to_check}/robots.txt"
     doc = Nokogiri::HTML(open("http://#{address_to_check}")) rescue false
-    #doc = Nokogiri::HTML(open("http://www.onet.pl")) rescue false
     
     #pobieramy plik robots.txt, zamieniamy na małe litery, usuwamy spacje
-	if doc
-		robots_txt = open(robots_address).read.downcase.gsub(" ", "") rescue false
-		  #@robots_txt = robots_uri.read
-    @h1_msg = check_h1(doc) #1ok
-    @title_msg = check_title(doc) #1 ok
-    @desc_msg = check_description(doc) #1ok
-    @robots_msg = check_robots(doc) # 0 ok
-    #jeszcze robots case sens
-    if robots_txt
-      @rtxt_msg = check_robotstxt(robots_txt) #0ok
-    else
-			@rtxt_msg = 0 #there is no robots.txt so there is no disalow for bot
+    if doc
+      robots_txt = open(robots_address).read.downcase.gsub(" ", "") rescue false
+      @h1_msg = check_h1(doc) #1ok
+      @title_msg = check_title(doc) #1 ok
+      @desc_msg = check_description(doc) #1ok
+      @robots_msg = check_robots(doc) # 0 ok
+    
+      if robots_txt
+        @rtxt_msg = check_robotstxt(robots_txt) #0ok
+      else
+	@rtxt_msg = 0 #there is no robots.txt so there is no disalow for bot
+      end
+
+      @r301_msg = check_301_redirection(address_to_check) # OK @r301_msg['redirection_301_status'] == 1 
+      @wildcard_msg = check_wildcard(address_to_check) # 0 OK
+      @https_msg = check_https_copy(address_to_check) # 0 OK
+      @w3_results = check_w3val(address_to_check) # OK @w3_results['w3_errors']<1
+      
+      images = get_images(doc)
+      @images_no_alt = check_images_no_alt(images) # OK 0
+      @images_empty_alt = check_images_empty_alt(images) # OK 0
+    
+      @page_speed = get_pagespeed(address_to_check) #dodokonczenia # OK > 80
+      @alexa = get_alexa_rank(address_to_check)
+      @page_rank = get_page_rank(address_to_check) #z proxy moze byc problem
+      @site = get_site_nr(address_to_check)
+
+      @temp_address_to_check = address_to_check
+
+      moz_data = get_moz_data(address_to_check)	
+	if moz_data
+   	  @pda = moz_data.fetch("pda")  
+	  @ueid = moz_data.fetch("ueid")
+	  @uid = moz_data.fetch("uid")
+	  @upa = moz_data.fetch("upa")
+	end
+
+      @final_score = count_seo_score(@h1_msg, @title_msg, @desc_msg, @robots_msg, @rtxt_msg, @r301_msg, @wildcard_msg, @https_msg, @w3_results, @images_no_alt, @images_empty_alt, @page_speed)
+     end
+  end
+
+  def count_seo_score(h1_msg, title_msg, desc_msg, robots_msg, rtxt_msg, r301_msg, wildcard_msg, https_msg, w3_results, images_no_alt, images_empty_alt, page_speed)
+    score = 0
+
+    if h1_msg == 1
+      score += 10
+      h1_error = 0
+    else 
+      h1_error = 1	
     end
 
-    @r301_msg = check_301_redirection(address_to_check) # OK @r301_msg['redirection_301_status'] == 1 
-    @wildcard_msg = check_wildcard(address_to_check) # 0 OK
-    @https_msg = check_https_copy(address_to_check) # 0 OK
-    @w3_results = check_w3val(address_to_check) # OK @w3_results['w3_errors']<1
-        # =>     w3_results = {'w3_errors' => w3, 'info_address' => val_address}
-    
-    images = get_images(doc)
-    @images_no_alt = check_images_no_alt(images) # OK 0
-    @images_empty_alt = check_images_empty_alt(images) # OK 0
-    
-    @page_speed = get_pagespeed(address_to_check) #dodokonczenia # OK > 80
-    @alexa = get_alexa_rank(address_to_check)
-    @page_rank = get_page_rank(address_to_check) #z proxy moze byc problem
-    @site = get_site_nr(address_to_check)
+    if title_msg == 1
+      score += 10
+      title_error = 0
+    else
+      title_error = 1
+    end
 
-    @temp_address_to_check = address_to_check
+    if desc_msg == 1
+      score += 5
+      desc_error = 0
+    else 
+      desc_error = 1
+    end
 
-    moz_data = get_moz_data(address_to_check)	
-		if moz_data
-			@pda = moz_data.fetch("pda")  
-			@ueid = moz_data.fetch("ueid")
-			@uid = moz_data.fetch("uid")
-			@upa = moz_data.fetch("upa")
-		end
+    if robots_msg == 1
+      robots_error = 1
+    else 
+      score += 10
+      robots_error = 0
+    end
 
-		@final_score = count_seo_score(@h1_msg, @title_msg, @desc_msg, @robots_msg, @rtxt_msg, @r301_msg, @wildcard_msg, @https_msg, @w3_results, @images_no_alt, @images_empty_alt, @page_speed)
-	end
-    #jeszcze czy jeden title
-    #czy jeden desc
-	end
+    if rtxt_msg == 0
+      rtxt_error = 0
+      score += 10
+    else 
+      rtxt_error = 1
+    end
 
-	def count_seo_score(h1_msg, title_msg, desc_msg, robots_msg, rtxt_msg, r301_msg, wildcard_msg, https_msg, w3_results, images_no_alt, images_empty_alt, page_speed)
-		score = 0
+    if r301_msg['redirection_301_status'] == 1
+      score += 10
+      r301_error = 0
+    else 
+      r301_error = 1
+    end
 
-		if h1_msg == 1
-			score += 10
-			h1_error = 0
-		else 
-			h1_error = 1	
-		end
+    if wildcard_msg == 0
+      score += 10
+      wildcard_error = 0
+    else 
+      wildcard_error = 1
+    end
 
-		if title_msg == 1
-			score += 10
-			title_error = 0
-		else
-			title_error = 1
-		end
+    if https_msg == 0
+      score += 10
+      https_error = 0
+    else 
+      https_error = 1
+    end
 
-		if desc_msg == 1
-			score += 5
-			desc_error = 0
-		else 
-			desc_error = 1
-		end
+    if w3_results['w3_errors'] < 1
+      score += 5
+      w3_error = 0
+    else
+      w3_error = 1
+    end
 
-		if robots_msg == 1
-			robots_error = 1
-			else 
-			score += 10
-			robots_error = 0
-		end
+    if images_no_alt.blank?
+      score += 5
+      images_no_alt_error = 0
+    else 
+      images_no_alt_error = 1
+    end
 
-		if rtxt_msg == 0
-			rtxt_error = 0
-			score += 10
-		else 
-			rtxt_error = 1
-		end
+    if images_empty_alt.blank?
+      score += 5
+      images_empty_alt_error = 0
+    else
+      images_empty_alt_error = 1
+    end
 
-		if r301_msg['redirection_301_status'] == 1
-			score += 10
-			r301_error = 0
-		else 
-			r301_error = 1
-		end
+    if page_speed > 80 
+      score += 10
+      page_speed_error = 0
+    else 
+      page_speed_error = 1
+    end
 
-		if wildcard_msg == 0
-			score += 10
-			wildcard_error = 0
-		else 
-			wildcard_error = 1
-		end
-
-		if https_msg == 0
-			score += 10
-			https_error = 0
-		else 
-			https_error = 1
-		end
-
-		if w3_results['w3_errors'] < 1
-			score += 5
-			w3_error = 0
-		else
-			w3_error = 1
-		end
-
-		if images_no_alt.blank?
-			score += 5
-			images_no_alt_error = 0
-		else 
-			images_no_alt_error = 1
-		end
-
-		if images_empty_alt.blank?
-			score += 5
-			images_empty_alt_error = 0
-		else
-			images_empty_alt_error = 1
-		end
-
-		if page_speed > 80 
-			score += 10
-			page_speed_error = 0
-		else 
-			page_speed_error = 1
-		end
-
-		score_results = Hash.new
+    score_results = Hash.new
     score_results = {
     	'score' => score, 
     	'h1_error' => h1_error, 
@@ -164,68 +155,19 @@ class TestsController < ApplicationController
     	'robots_error' => robots_error,
     	'rtxt_error' => rtxt_error,
     	'r301_error' => r301_error,
-			'wildcard_error' => wildcard_error,
-			'https_error' => https_error,
-			'w3_error' => w3_error,
-			'images_empty_alt_error' => images_empty_alt_error,
-			'images_no_alt_error' => images_no_alt_error,
-			'page_speed_error' => page_speed_error    	
+	'wildcard_error' => wildcard_error,
+	'https_error' => https_error,
+	'w3_error' => w3_error,
+	'images_empty_alt_error' => images_empty_alt_error,
+	'images_no_alt_error' => images_no_alt_error,
+	'page_speed_error' => page_speed_error    	
     }
 
-		return score_results
-
-	end
-
-  def title
-#api
-#AIzaSyBJcAvAu1Dz9Bk4x7xn1b0Yg9PSVJkMrU4   
-#speed_info = open("https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=http://scarto.pl/&key=AIzaSyBJcAvAu1Dz9Bk4x7xn1b0Yg9PSVJkMrU4").read
-#jdoc = JSON.parse(speed_info)
-#@ps = jdoc.fetch("title")  
+  return score_results
 
   end
-
-
-  def temp
-   one = "http://www.transfermarkt.de/oscar/profil/spieler/85314"  
-   val_address = "http://www.transfermarkt.de/fc-chelsea/startseite/verein/631/saison_id/2014"
-   w3doc = Nokogiri::HTML(open(one)) rescue false
-  #alx = alexa_doc.xpath('//div[@class="rank-row"]').xpath('//strong[@class="metrics-data align-vmiddle"]').first.to_s.gsub(/[^0-9]/, '').to_i
-   
-    if w3doc       
-      @players = w3doc.xpath('//td[@class="hauptlink"]')
-      player_name = w3doc.xpath('//div[@class="spielername-profil"]/text()')
-      @data = w3doc.xpath('//table[@class="profilheader"]/tr')
-      @player_array = Array.new
-      @player_array.clear
-      @player_array.push(player_name)
-
-      @data.each do |z|
-        if z.to_s.include?("Geburtsdatum")
-          after = z.xpath('//td[@class="wsnw"]/text()')    
-          @player_array.push(after)      
-        elsif z.to_s.include?("Marktwert")
-          @player_array.push(z.to_s.gsub(/[^0-9^,]/, ''))
-        elsif z.to_s.include?("Nationalität")
-          after = z.content.to_s.gsub("Nationalität:","")    
-          @player_array.push(after)        
-        elsif z.to_s.include?("Größe")
-          #@player_array.push(z) 
-        end
-      end
-      #CSV.open("file1.csv", "w+") do |csv|
-      #      csv << [z]
-      #end
-    
-    end
-  end
-
-  def temp2
 
  
-  end
-
-
 private
 
   # redirection check - there should be a 301 redirection 
@@ -233,10 +175,8 @@ private
   # www -> 301 -> non www
   def check_301_redirection(address)
 
-	#todo - jeszcze jak jest subka podana to sprawdzenie robić dla subki - maklerska.opole.pl
-
-  	non_www_version = get_http_status(address)
-  	www_version = get_http_status("www.#{address}")
+    non_www_version = get_http_status(address)
+    www_version = get_http_status("www.#{address}")
     
     if non_www_version != '301' && www_version != '301'
       r301_status = 0
@@ -319,44 +259,45 @@ private
   	# meta name="DESCRIPTION"
   	# if none is found, then there is an error 
     meta_names = Array["Description", "description", "DESCRIPTION"]
-	  @meta_errors = 0
-	  meta_names.each do |q|
-	    all_desc = doc.at("meta[@name=\"#{q}\"]")
-	    if all_desc #if there is a meta description, go on
-		    @description_full = all_desc['content']
-		    if @description_full
-			    description = @description_full
-		 	    if description.blank? #we check if desc is empty
-			      desc_msg = 0
-		      else #if not, we want to make sure that descriptions length is proper
-		        desc_chars = description.length
-		          if desc_chars < 150 and desc_chars > 1
-		      	    desc_msg = 1 #length OK
-		          elsif desc_chars > 150
-		      	    desc_msg = 2 # too long
-		          elsif desc_chars <1
-		      	    desc_msg = 0 #empty
-		          end	
-		      end
-		      return desc_msg
-		    end  
-	    else
-	    	# as staded before, we look for 3 variants (description, Description, DESCRIPTION)
-	    	# if one is found (correct), meta errors will be 2
-	    	# if none is found (no description found at all) then meta errors will be 3
-	    	# if meta_errors will be less then 2, then it is possible that there are two diff descriptions ;)
-	    	@meta_errors += 1
-	    end
-	 end
+      @meta_errors = 0
+      meta_names.each do |q|
+      all_desc = doc.at("meta[@name=\"#{q}\"]")
+	    
+      if all_desc #if there is a meta description, go on
+	@description_full = all_desc['content']
+	if @description_full
+	  description = @description_full
+	  if description.blank? #we check if desc is empty
+	    desc_msg = 0
+	  else #if not, we want to make sure that descriptions length is proper
+	    desc_chars = description.length
+            if desc_chars < 150 and desc_chars > 1
+	      desc_msg = 1 #length OK
+	    elsif desc_chars > 150
+	      desc_msg = 2 # too long
+	    elsif desc_chars <1
+	      desc_msg = 0 #empty
+	    end	
+	  end
+	return desc_msg
+	end  
+      else
+	 # as staded before, we look for 3 variants (description, Description, DESCRIPTION)
+	 # if one is found (correct), meta errors will be 2
+	 # if none is found (no description found at all) then meta errors will be 3
+	 # if meta_errors will be less then 2, then it is possible that there are two diff descriptions ;)
+	 @meta_errors += 1
+      end
+    end
   end
 
   def check_h1(doc) 
       # h1_tag = doc.at("h1") <- this will only find <h1>text</h1> but sometimes
       # we have sth. like <h1><a href="">bla bla</a></h1> and doc.at don't read it properly
       # xpath solves this problem because it doesn't care about h1 content 
-	    h1_tag = doc.xpath('//h1')
-	    if h1_tag.blank?
-	  	h1_msg = 0
+	  h1_tag = doc.xpath('//h1')
+	  if h1_tag.blank?
+	    h1_msg = 0
 	  else
 	    h1_nr = h1_tag.count
 	    if h1_nr == 1 
